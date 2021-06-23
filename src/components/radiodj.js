e6bfc1fea1220cdf1ba8f5d454d3f388fae6dc71
stebogit <stefano.borghi@gmail.com>
2021-06-23 01:49:37 -0700

    fixes download and fixes #1

--- src/components/radiodj.js
+++ src/components/radiodj.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const URL = 'https://media.deejay.it';
+
 const deejay = axios.create({
-  baseURL: 'https://media.deejay.it',
   validateStatus: (status) => status < 400,
 });
 
@@ -24,10 +25,10 @@ export function getDJCI(date) {
   const headers = getHeaders(date, 'Deejay Chiama Italia', 'djci');
   return [
     deejay.head(
-      `/${format(date, '/')}/episodes/deejay_chiama_italia/deejay_chiama_italia-${format(date)}.mp3`,
+      `${URL}/${format(date, '/')}/episodes/deejay_chiama_italia/deejay_chiama_italia-${format(date)}.mp3`,
       headers,
     ),
-    deejay.head(`/${format(date, '/')}/episodes/deejay_chiama_italia/${format(date)}.mp3`, headers),
+    deejay.head(`${URL}/${format(date, '/')}/episodes/deejay_chiama_italia/${format(date)}.mp3`, headers),
   ];
 }
 
@@ -35,8 +36,8 @@ export function getNoSpoiler(date) {
   // https://media.deejay.it/2021/06/20/episodes/no_spoiler/no_spoiler-20210620.mp3
   const headers = getHeaders(date, 'No Spoiler', 'nosp');
   return [
-    deejay.head(`/${format(date, '/')}/episodes/no_spoiler/no_spoiler-${format(date)}.mp3`, headers),
-    deejay.head(`/${format(date, '/')}/episodes/no_spoiler/${format(date)}.mp3`, headers),
+    deejay.head(`${URL}/${format(date, '/')}/episodes/no_spoiler/no_spoiler-${format(date)}.mp3`, headers),
+    deejay.head(`${URL}/${format(date, '/')}/episodes/no_spoiler/${format(date)}.mp3`, headers),
   ];
 }
 
@@ -44,7 +45,10 @@ export function getVolo(date) {
   // https://media.deejay.it/2020/08/21/episodes/il_volo_del_mattino/il_volo_del_mattino-20200821.mp3
   const headers = getHeaders(date, 'Il Volo del Mattino', 'volo');
   return [
-    deejay.head(`/${format(date, '/')}/episodes/il_volo_del_mattino/il_volo_del_mattino-${format(date)}.mp3`, headers),
-    deejay.head(`/${format(date, '/')}/episodes/il_volo_del_mattino/${format(date)}.mp3`, headers),
+    deejay.head(
+      `${URL}/${format(date, '/')}/episodes/il_volo_del_mattino/il_volo_del_mattino-${format(date)}.mp3`,
+      headers,
+    ),
+    deejay.head(`${URL}/${format(date, '/')}/episodes/il_volo_del_mattino/${format(date)}.mp3`, headers),
   ];
 }
